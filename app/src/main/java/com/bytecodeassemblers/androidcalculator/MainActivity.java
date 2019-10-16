dec83e6bb4ac497f371169e7a1b5a7d109c01970
Anastasios Zarogoulidis <anastasiszaro@gmail.com>
2019-10-17 00:24:53 +0300

    Added a new button that does nothing

--- app/src/main/java/com/bytecodeassemblers/androidcalculator/MainActivity.java
+++ app/src/main/java/com/bytecodeassemblers/androidcalculator/MainActivity.java
@@ -3,12 +3,17 @@ package com.bytecodeassemblers.androidcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button newButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newButton = findViewById(R.id.button);
     }
 }
